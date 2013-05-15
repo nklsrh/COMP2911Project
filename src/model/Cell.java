ce721f8a66d80a853d15a7f7dd648425fad98bbf
Ryan Tan <ryantanjunming@gmail.com>
2013-05-15 23:32:18 +1000

    - fixed what nikhill did to all my files… arggg
    ------------------------------------------ Stuff To Do -----------------------------------------------
    - Write
    - Write Ryan's Diary
    - Write Class Files
    - Draw UML

---
+++ src/model/Cell.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Cell implements CellInterface {
+	
+	private Integer number;
+	private boolean isFixed;
+	
+	//for non-changing cell
+	public Cell(int number) {
+		this.number = number;
+		this.isFixed = true;
+	}
+	
+	//for a changing cell
+	public Cell() {
+		this.number = null;
+		this.isFixed = false;
+	}
+	
+	public boolean isFixed(){
+		return isFixed;
+	}
+	
+	@Override
+	public int getNumber() {
+		return number;
+	}	
+}
