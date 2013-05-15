336f2249abaf7b84d99a86700cea9bff61eb256b
Ryan Tan <ryantanjunming@gmail.com>
2013-05-15 20:28:06 +1000

    ---------------------------------------- Commit Message ----------------------------------------
    - Changed Cell.java and CellInterface, was too complicated and not OO enough
    	* cel should only know if it is a fixed number (permanent)  and its own number
    	* it should not know its own position..
    
    ------------------------------------------ Stuff To Do -----------------------------------------------
    
    - Write
    - Write Ryan's Diary
    - Write Class Files
    - Draw UML

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,62 +1,27 @@
 package model;
 
 public class Cell implements CellInterface {
-	private int number;
-	private int columnPos;
-	private int rowPos;
-	private int index;
-	private boolean isSolved;
-	private boolean isWrong;
-	private boolean isFilled;
 	
-	public Cell(int number, int columnPos, int rowPos, int index, boolean isSolved) {
+	private Integer number;
+	private boolean isFixed;
+	
+	//for non-changing cell
+	public Cell(int number) {
 		this.number = number;
-		this.columnPos = columnPos;
-		this.rowPos = rowPos;
-		this.index = index;
-		this.isSolved = isSolved;
-		if (0 < number && number < 10)
-			isFilled = true;
-		else
-			isFilled = false;
-		if (isSolved)
-			isWrong = false;
-		else 
-			isWrong = true;
+		this.isFixed = true;
+	}
+	
+	//for a changing cell
+	public Cell() {
+		this.number = null;
+	}
+	
+	public boolean isFixed(){
+		return isFixed;
 	}
 	
 	@Override
 	public int getNumber() {
 		return number;
-	}
-
-	@Override
-	public int getColumnPos() {
-		return columnPos;
-	}
-
-	@Override
-	public int getRowPos() {
-		return rowPos;
-	}
-
-	@Override
-	public int getIndex() {
-		return index;
-	}
-	
-	@Override
-	public Boolean isSolved() {
-		return isSolved;
-	}
-	
-	@Override
-	public Boolean isFilled() {
-		return isFilled;
-	}
-
-	@Override
-	public Boolean isWrong() {
-		return isWrong;
 	}	
 }
