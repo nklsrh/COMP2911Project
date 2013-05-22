82118592a978030006d8ae7c4cd337536a853375
Ryan Tan <ryantanjunming@gmail.com>
2013-05-23 00:57:49 +1000

    -Fixed Models Grid, Row and Column -Fixed how Puzzel interacts in placing Grids over.
    ------------------------------------------ Stuff To Do -----------------------------------------------
    - Write Ryan's Diary
    - Review Dancing Links for Sudoku generation
    - Build Dancing Links Hodoku

--- src/model/Column.java
+++ src/model/Column.java
@@ -2,19 +2,34 @@ package model;
 
 import java.util.ArrayList;
 
-public class Column {
+public class Column implements RowColumnInterface{
 	private ArrayList<Cell> columnCells;
-	protected static final int NUM_COLUMN_SIDE = 9;
+	protected static final int NUMBER_OF_CELLS = 9;
 	
 	public Column() {
 		columnCells = new ArrayList<Cell>();
 	}
 	
-	public ArrayList<Cell> getColumn() {
+	public ArrayList<Cell> getList() {
 		return columnCells;
 	}
 	
-	public void addCell(int index, Cell cell) {
-		columnCells.add(index, cell);
+	@Override
+	public void addToList(Cell cell) {
+		if(columnCells.size() <= NUMBER_OF_CELLS){
+			columnCells.add(cell);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer string = new StringBuffer();
+		string.append("| ");
+		if(!columnCells.isEmpty()) {
+			for(int i=0; i< NUMBER_OF_CELLS; i++) {
+				string.append(columnCells.get(i).getNumber() + " | ");	
+			}
+		}
+		return string.toString();
 	}
 }
