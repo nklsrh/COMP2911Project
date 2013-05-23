eac6a63d543dfce2cedfd0507c75e828dabea294
Ryan Tan <ryantanjunming@gmail.com>
2013-05-23 14:11:35 +1000

    no message

--- src/controller/Generator.java
+++ src/controller/Generator.java
@@ -1,5 +1,13 @@
 package controller;
 
+import java.util.Random;
+import model.Puzzle;
+
 public class Generator {
+	
+	//fail end-case for tries
+	private static final int MAX_TRIES = 1000000;
+	private static Puzzle sudoku = new Puzzle();
+	
 	
 }
