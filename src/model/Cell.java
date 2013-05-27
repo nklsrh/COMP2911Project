d530a6d4ffc777d6215dae7656bb362d9a456f3b
Nicholas Ho <terran324@gmail.com>
2013-05-27 10:27:26 +1000

    Added javadoc documentation to Cell.java

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -31,34 +31,72 @@ public class Cell {
 		this.isEmpty = true;
 	}
 	
+	/**
+	 * A boolean for checking if a cell of a Sudoku game is non-changing/fixed
+	 * @return true if fixed, false otherwise
+	 */
 	public boolean isFixed(){
 		return isFixed;
 	}
+	
+	/**
+	 * A boolean for checking if a cell of  a Sudoku game is empty.
+	 * @return true if empty, false otherwise.
+	 */
 	public boolean isEmpty(){
 		return isEmpty;
 	}
 	
+	/**
+	 * A method to set a cell's isFixed flag to true
+	 * @param set determines if a particular cell is a fixed cell or not.
+	 */
 	public void setFixed(boolean set){
 		this.isFixed = set;
 	}
 	
+	/**
+	 * A getter that returns the number contained in a particular cell, regardless of whether it is correct or
+	 * wrong.
+	 * @return number contained in the cell.
+	 */
 	public Integer getNumber() throws NullPointerException{
 		return this.number;
 	}
 	
+	/**
+	 * A getter that returns the correct solution to the number that should be contained in a particular cell.
+	 * @return correct solution of the cell
+	 */
 	public Integer getSolution() throws NullPointerException{
 		return this.solution;
 	}
 	
+	/**
+	 * A setter that gives a number (not necessarily correct) to a particular cell. If the cell used to be 
+	 * empty, its isEmpty flag is disabled to false.
+	 * @param num the number to be added to the cell.
+	 */
 	public void setNumber(int num) throws NullPointerException{
 		this.number = num;
 		this.isEmpty = false;
 	}
 	
+	/**
+	 * A setter that gives a number to a particular cell. This number is correct and is part of the solution
+	 * to the Sudoku puzzle.
+	 * @param num the number solution to be added to the cell
+	 * @throws NullPointerException
+	 */
 	public void setSolution(int num) throws NullPointerException{
 		this.solution = num;
 	}
 	
+	/**
+	 * This method compares the number placed by a user with the predefined solution of the Sudoku game, to
+	 * see if the user has filled the cell with the correct number.
+	 * @return true if the user has filled the cell correctly, false otherwise.
+	 */
 	public boolean checkNumberSolution()
 	{
 		boolean result = false;
@@ -73,6 +111,9 @@ public class Cell {
 		return result;
 	}
 	
+	/**
+	 * This method resets a cell such that it is completely empty.
+	 */
 	public void nullCell(){
 		this.number = null;
 		this.isEmpty = true;
