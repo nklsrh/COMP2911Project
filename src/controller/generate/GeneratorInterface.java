81cd4d1954ca1b38d49e10058e15452c558a23eb
Ryan Tan <ryantanjunming@gmail.com>
2013-06-02 22:14:40 +1000

    - JDOC more stuff
    ----------------------------------- Stuff To Do ---------------------------------

--- src/controller/generate/GeneratorInterface.java
+++ src/controller/generate/GeneratorInterface.java
@@ -2,8 +2,18 @@ package controller.generate;
 
 import model.Puzzle;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface for the GeneratorInterface.
+ */
 public interface GeneratorInterface {
 	
+	/**
+	 * Packages up a puzzle object and fills all cell solutions.
+	 *
+	 * @param puzzle the puzzle
+	 * @return the puzzle
+	 */
 	public Puzzle packageUp(Puzzle puzzle);
 	
 }
